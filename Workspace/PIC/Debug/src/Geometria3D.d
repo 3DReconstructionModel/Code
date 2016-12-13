47f019d628d8cf2997fed6a4a99b58187ac2870d
irenecortes <irenecorteslafuente@gmail.com>
2016-12-13 12:12:16 +0100

    modificado:    .cproject 	modificado:    Debug/PIC 	modificado:    Debug/src/Geometria3D.d 	modificado:    Debug/src/Geometria3D.o 	modificado:    Debug/src/subdir.mk 	modificado:    src/Geometria3D.cpp

--- Workspace/PIC/Debug/src/Geometria3D.d
+++ Workspace/PIC/Debug/src/Geometria3D.d
@@ -1,1 +1,7 @@
-src/Geometria3D.o: ../src/Geometria3D.cpp
+src/Geometria3D.o: ../src/Geometria3D.cpp \
+ /home/irenerrrd/opencv_contrib/modules/xfeatures2d/include/opencv2/xfeatures2d.hpp \
+ /home/irenerrrd/opencv_contrib/modules/xfeatures2d/include/opencv2/xfeatures2d/nonfree.hpp
+
+/home/irenerrrd/opencv_contrib/modules/xfeatures2d/include/opencv2/xfeatures2d.hpp:
+
+/home/irenerrrd/opencv_contrib/modules/xfeatures2d/include/opencv2/xfeatures2d/nonfree.hpp:
