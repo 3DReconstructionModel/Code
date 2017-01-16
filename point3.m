3436f1635e423145586befa0c5c11998c29da57d
irenecortes <irenecorteslafuente@gmail.com>
2017-01-16 13:15:02 +0100

    Add files via upload

---
+++ point3.m
@@ -0,0 +1,27 @@
+figure
+plot3(points1(:,1), points1(:,2), points1(:,3), '.r')
+hold on;
+axis equal
+% xlim([-10,10])
+% ylim([-10, 5])
+% zlim([2,20])
+
+xlim([-5,4])
+ylim([-5,3])
+zlim([3,10])
+pause(0.5);
+plot3(points2(:,1), points2(:,2), points2(:,3), '.g')
+pause(0.5);
+plot3(points3(:,1), points3(:,2), points3(:,3), '.b')
+pause(0.5);
+plot3(points4(:,1), points4(:,2), points4(:,3), '.k')
+pause(0.5);
+plot3(points5(:,1), points5(:,2), points5(:,3), '.m')
+pause(0.5);
+plot3(points6(:,1), points6(:,2), points6(:,3), '.c')
+pause(0.5);
+plot3(points7(:,1), points7(:,2), points7(:,3), '.y')
+pause(0.5);
+plot3(points8(:,1), points8(:,2), points8(:,3), '.r')
+pause(0.5);
+plot3(points9(:,1), points9(:,2), points9(:,3), '.g')
